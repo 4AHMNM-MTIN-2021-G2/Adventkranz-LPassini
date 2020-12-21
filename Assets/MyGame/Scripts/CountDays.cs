8bdff669c4f3f85315d4b5cae19f45e33138b541
LeonhardPassini <leonhard.passini03@htl-salzburg.ac.at>
2020-12-21 10:42:59 +0100

    Update Script

--- Assets/MyGame/Scripts/CountDays.cs
+++ Assets/MyGame/Scripts/CountDays.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDays : MonoBehaviour
 {
+
+
+
+    public Text DaysGui;
+    private System.DateTime christmasDay, currentDay;
+
     System.DateTime firstAdvent;
     System.DateTime secondAdvent;
     System.DateTime thirdAdvent;
     System.DateTime fourthAdvent;
-    System.DateTime christmasDay;
-    
+    // System.DateTime christmasDay;
+    private int days = 0;
+    private System.DateTime currTestDay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +25,17 @@ public class CountDays : MonoBehaviour
         thirdAdvent = new System.DateTime(2020, 12, 13);
         fourthAdvent = new System.DateTime(2020, 12, 20);
         christmasDay = new System.DateTime(2020, 12, 24);
-    }
 
-    // Update is called once per frame
-    void Update()
+        currentDay = System.DateTime.Today;
+        TotalDaysToChristmas();
+    }
+    private void TotalDaysToChristmas()
     {
-        
+        while (currentDay.CompareTo(christmasDay) < 0)
+        {
+            currentDay = currentDay.AddDays(1);
+            days++;
+        }
+       DaysGui.text = days.ToString();
     }
 }
